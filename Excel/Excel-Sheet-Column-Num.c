7a8268bf6df182e00bb75cf3d730678a262c7a56
NVk925 <khoanv.23bi14223@usth.edu.vn>
2024-12-05 20:07:38 +0700

    Update Excel-Sheet-Column-Num.c

--- Excel/Excel-Sheet-Column-Num.c
+++ Excel/Excel-Sheet-Column-Num.c
@@ -19,3 +19,33 @@ int main() {
     printf("Column Title: %s, Column Number: %d\n", columnTitle, columnNumber);
     return 0;
 }
+/*Rcursive O(n)
+#include <stdio.h>
+#include <string.h>
+
+int titleToNumberHelper(char* columnTitle, int index, int length) {
+    // Base case: when we've processed all characters
+    if (index == length) {
+        return 0;
+    }
+
+    // Recursive step: Get the value of the current character
+    int currentValue = columnTitle[index] - 'A' + 1;
+    
+    // Process the rest of the string
+    return currentValue * (1 << (26 * (length - index - 1))) + titleToNumberHelper(columnTitle, index + 1, length);
+}
+
+int titleToNumber(char* columnTitle) {
+    int length = strlen(columnTitle);
+    return titleToNumberHelper(columnTitle, 0, length);
+}
+
+// Example usage
+int main() {
+    char columnTitle[] = "ZY"; // Example: Column "ZY"
+    int columnNumber = titleToNumber(columnTitle);
+    printf("Column Title: %s, Column Number: %d\n", columnTitle, columnNumber);
+    return 0;
+}
+*/
